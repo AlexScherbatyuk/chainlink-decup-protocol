8260a0750738cf04851e1ab5f588b8bc1d61d910
Alexandr Shcherbatyuk <a.shcherbatyuk@gmail.com>
2025-06-27 16:13:38 +0300

    Allowed tokens

--- frontend/lib/contracts/addresses.ts
+++ frontend/lib/contracts/addresses.ts
@@ -1,4 +1,4 @@
-export const contractAddresses = {
+const getContractAddresses = {
     sepolia: {
         DeCup: "0x0e3da2602e33acb76f925f379dc7b2e1c5d4fefb",
         DeCupManager: "0xa5d8d7950bcf65be5fed367cc618e7118c9d81d6",
@@ -8,3 +8,19 @@ export const contractAddresses = {
         DeCupManager: "0x0000000000000000000000000000000000000000", // Not deployed yet
     }
 } as const
+
+const getTokenAddresses = {
+    sepolia: {
+        WETH: "0xdd13E55209Fd76AfE204dBda4007C227904f0a81",
+        USDC: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+    },
+    avalancheFuji: {
+        WAVAX: "0xd00ae08403B9bbb9124bB305C09058E32C39A48c",
+        USDC: "0x5425890298aed601595a70AB815c96711a31Bc65"
+    }
+} as const
+
+export {
+    getContractAddresses,
+    getTokenAddresses
+}
--- frontend/lib/contracts/index.ts
+++ frontend/lib/contracts/index.ts
@@ -1,9 +1,9 @@
 import DeCupABI from '../../../contracts/out/DeCup.sol/DeCup.json'
 import DeCupManagerABI from '../../../contracts/out/DeCupManager.sol/DeCupManager.json'
-import { contractAddresses } from './addresses'
+import { getContractAddresses, getTokenAddresses } from './addresses'
 
 export {
     DeCupABI,
     DeCupManagerABI,
-    contractAddresses
+    getContractAddresses,
 }
