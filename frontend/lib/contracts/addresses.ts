d9f1b1f1cb45656d25736c4abab003e1c59d0333
Alexandr Shcherbatyuk <a.shcherbatyuk@gmail.com>
2025-06-27 15:34:20 +0300

    abi-contract

---
+++ frontend/lib/contracts/addresses.ts
@@ -0,0 +1,10 @@
+export const contractAddresses = {
+    sepolia: {
+        DeCup: "0x0e3da2602e33acb76f925f379dc7b2e1c5d4fefb",
+        DeCupManager: "0xa5d8d7950bcf65be5fed367cc618e7118c9d81d6",
+    },
+    avalancheFuji: {
+        DeCup: "0x0000000000000000000000000000000000000000", // Not deployed yet
+        DeCupManager: "0x0000000000000000000000000000000000000000", // Not deployed yet
+    }
+} as const
---
+++ frontend/lib/contracts/index.ts
@@ -0,0 +1,9 @@
+import DeCupABI from '../../../contracts/out/DeCup.sol/DeCup.json'
+import DeCupManagerABI from '../../../contracts/out/DeCupManager.sol/DeCupManager.json'
+import { contractAddresses } from './addresses'
+
+export {
+    DeCupABI,
+    DeCupManagerABI,
+    contractAddresses
+}
